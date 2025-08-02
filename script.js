7405dffc6df5c3e2525b61d79b7812ed1fe9a57d
AndersLemme <a.lemme@hotmail.com>
2025-08-02 11:38:12 +0200

    Created function for add, subtract, multiply and divide

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("test");
+let a = 4;
+let b = 7;
+
+console.log(add(a,b));
+console.log(substract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
+
+
+function add(a, b){
+    return a + b;
+}
+function substract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}

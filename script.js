c7d083b354aefda88292c85e0a59f2aad30fdb2c
AndersLemme <a.lemme@hotmail.com>
2025-08-21 22:08:19 +0200

    Changed result to be string and append each number. Sum operator just displays number of the string in console.log. Did some changes to the styling of buttons and display. I have to make it so that it is only possible to add 1 comma, and i need to make the display have border radius on top.

--- script.js
+++ script.js
@@ -3,62 +3,76 @@ let b = 7;
 let operator = "-";
 
 let sum = 0
+let display = ""
 
 console.log(operate(a, b, operator));
 const result = document.querySelector(".result");
-result.value = sum;
+result.value = display;
 
 
 const one_btn = document.querySelector(".one");
 one_btn.addEventListener("click", () => { 
-    sum += 1;
-    result.value = sum;
+    display += "1";
+    result.value = display;
 });
 const two_btn = document.querySelector(".two");
 two_btn.addEventListener("click", () => { 
-    sum += 2;
-    result.value = sum;
+    display += "2";
+    result.value = display;
 });
 const three_btn = document.querySelector(".three");
 three_btn.addEventListener("click", () => { 
-    sum += 3;
-    result.value = sum;
+    display += "3";
+    result.value = display;
     });
 const four_btn = document.querySelector(".four");
 four_btn.addEventListener("click", () => { 
-    sum += 4;
-    result.value = sum;
+    display += "4";
+    result.value = display;
 });
 const five_btn = document.querySelector(".five");
 five_btn.addEventListener("click", () => { 
-    sum += 5;
-    result.value = sum;
+    display += "5";
+    result.value = display;
 });
 const six_btn = document.querySelector(".six");
 six_btn.addEventListener("click", () => { 
-    sum += 6;
-    result.value = sum;
+    display += "6";
+    result.value = display;
 });
 const seven_btn = document.querySelector(".seven");
 seven_btn.addEventListener("click", () => { 
-    sum += 7;
-    result.value = sum;
+    display += "7";
+    result.value = display;
 });
 const eight_btn = document.querySelector(".eight");
 eight_btn.addEventListener("click", () => { 
-    sum += 8;
-    result.value = sum;
+    display += "8";
+    result.value = display;
 });
 const nine_btn = document.querySelector(".nine");
 nine_btn.addEventListener("click", () => { 
-    sum += 9;
-    result.value = sum;
+    display += "9";
+    result.value = display;
 });
 const zero_btn = document.querySelector(".zero");
 zero_btn.addEventListener("click", () => { 
-    sum += 0;
-    result.value = sum;
+    display += "0";
+    result.value = display;
 });
+
+// OPERATORS 
+const comma_btn = document.querySelector(".comma");
+comma_btn.addEventListener("click", () => {
+    display += "."
+    result.value = display;
+});
+
+const sum_btn = document.querySelector(".sum");
+sum_btn.addEventListener("click", () => {
+    sum = Number(display);
+    console.log(sum);
+})
 
 function operate(a, b, operator){
     //ans = 0;
